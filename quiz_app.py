83ab3fdde6cc8689b6f2b9a5928716f908a99885
astrofeldy <hannah.feldman@anu.edu.au>
2025-07-04 20:42:11 +1000

    Update quiz_app.py

--- quiz_app.py
+++ quiz_app.py
@@ -270,7 +270,7 @@ Enemies-turned-allies must survive alone in deep space, with queer romance and m
 
 PLACEHOLDER""",
 
-    "Crying in H Mart": """**Crying in H Mart**  
+    "Crying in H Mart": """**Crying in H Mart by SOMEONE**  
     
 *An emotional, reflective memoir.*
 
@@ -405,7 +405,7 @@ if st.button("Submit"):
 
         # Display personality match
         st.markdown(f"### You're a {personality_matches.get(top_book)}**")
-        st.markdown(f"### Your half-way-hero book match is: 🥇 **{top_book}**")
+        st.markdown(f"### 🥇 Your half-way-hero book match is: **{top_book}**")
         
         # Display header before book description
         st.markdown(f"**More about your half-way-hero book match:**")
