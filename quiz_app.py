38232787c554c36b5ac99dee6fde8a0cc3022da3
astrofeldy <hannah.feldman@anu.edu.au>
2025-07-04 18:21:26 +1000

    Create quiz_app.py

---
+++ quiz_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Paperbacks & Snacks Quiz", page_icon="📚")
+
+st.title("📚 Paperbacks and Snacks 6 Month Recs Quiz")
+st.write("Welcome to the highlights reel! Answer the following 10 very normal, very serious questions to find your next perfect read, based on the FB group thread of our half-way-highlights!")
